51fde79fc4148ddafa0a7ed613512108e03f4902
catherinessssss <lixiaoyin5@hotmail.com>
2016-02-02 23:40:58 +0800

    zhonggang homepage
    中港首页

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,34 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Zhonggang Homepage</title>
+	<link rel="stylesheet" type="text/css" href="/zg/Public/css/home.css">
+	<link rel="stylesheet" type="text/css" href="/zg/Public/css/common.css">
+	<link rel="stylesheet" type="text/css" href="/zg/Public/css/flexslider.css">
+	<script type="text/javascript" src="/zg/Public/js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="/zg/Public/js/jquery.flexslider-min.js"></script>
+	<script type="text/javascript" src="/zg/Public/js/home.js"></script>
+</head>
+<body>
+	<div class="top">
+		<span class="logo"></span>
+	</div>
+	<div class="flexslider">
+		<ul class="slides">
+			<li><img src="/zg/Public/images/page_1.png" alt="中港建筑机械租凭"></li>
+			<li><img src="/zg/Public/images/page_2.png" alt="中港建筑机械租凭"></li>
+			<li><img src="/zg/Public/images/page_3.png" alt="中港建筑机械租凭"></li>
+			<li><img src="/zg/Public/images/page_4.png" alt="中港建筑机械租凭"></li>
+		</ul>
+	</div>
+	<div class="menu">
+		<div class="intro"><span></span></div>
+		<div class="case"><span></span></div>
+		<div class="quali"><span></span></div>
+		<div class="setting"><span></span></div>
+		<div class="honor"><span></span></div>
+		<div class="tele"><span></span></div>
+	</div>
+</body>
+</html>
---
+++ Public/js/home.js
@@ -0,0 +1,8 @@
+$(function() { 
+    $(".flexslider").flexslider({
+            slideshowSpeed: 3000,
+            animationSpeed: 600,
+            directionNav: false
+        }
+    ); 
+});
